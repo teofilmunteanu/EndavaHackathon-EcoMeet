af44838ff1f5ccdbd0b4e5bd9e52eb1350ab7be8
Munteanu Teofil <andreiteofil01@gmail.com>
2022-09-16 12:59:32 +0300

    Added Chakra Themes to App

--- src/App.js
+++ src/App.js
@@ -7,13 +7,12 @@ import UpperHomePage from "./Components/UpperHomePage";
 import NavBar from "./Components/NavBar";
 import Description from "./Components/description";
 
-// 1. import `ChakraProvider` component
 import { ChakraProvider } from "@chakra-ui/react";
+import {mainTheme} from "./styles/theme";
 
 function App() {
-  // 2. Wrap ChakraProvider at the root of your app
   return (
-    <ChakraProvider>
+    <ChakraProvider resetCSS theme={mainTheme}>
       <NavBar></NavBar>
       <UpperHomePage></UpperHomePage>
       <Description></Description>
