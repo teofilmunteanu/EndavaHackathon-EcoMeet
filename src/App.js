85f3e708bbbc4443b1a46516d45db265e8e582b0
mitoconstantIN <constantinmitocaru22@gmail.com>
2022-09-16 14:44:00 +0300

    Routing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import * as React from "react";
 
 import UpperHomePage from "./Components/UpperHomePage";
 import NavBar from "./Components/NavBar";
+import { ChakraProvider } from "@chakra-ui/react";
+
 import Description from "./Components/description";
+import NavBar from "./Components/NavBar";
+import UpperHomePage from "./Components/UpperHomePage";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import {mainTheme} from "./styles/theme";
@@ -16,6 +20,19 @@ function App() {
       <NavBar></NavBar>
       <UpperHomePage></UpperHomePage>
       <Description></Description>
+
+import {Route, withRouter, Routes} from "react-router-dom"
+
+function App() {
+  return (
+    <ChakraProvider>
+      <React.Fragment>
+        <NavBar/>
+        <Routes>
+          <Route path="/home" element={<UpperHomePage/>}/>
+            
+        </Routes>
+      </React.Fragment>
     </ChakraProvider>
   );
 }
