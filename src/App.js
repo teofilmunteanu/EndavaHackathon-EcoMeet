cd10bb2d0adc915ab53e63e888b50bb6277eefae
Vlad Hailă <99145328+VladHaila@users.noreply.github.com>
2022-09-16 18:55:30 +0300

    Merge

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 import * as React from "react";
-import {Route, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 
 import Description from "./Components/description";
 import NavBar from "./Components/NavBar";
@@ -9,21 +9,24 @@ import UpperHomePage from "./Components/UpperHomePage";
 import BottomBar from "./Components/BottomBar";
 
 import { ChakraProvider } from "@chakra-ui/react";
-import {mainTheme} from "./styles/theme";
-
+import { mainTheme } from "./styles/theme";
 
 function App() {
   return (
     <ChakraProvider resetCSS theme={mainTheme}>
-        <NavBar/>
-        <Routes>
-          <Route path="/" element={<><UpperHomePage/><Description/></>}/>
-        </Routes>
-    <ChakraProvider>
-      <NavBar></NavBar>
-      <UpperHomePage></UpperHomePage>
-      <Description></Description>
-      <BottomBar></BottomBar>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <UpperHomePage />
+              <Description />
+            </>
+          }
+        />
+      </Routes>
+      <BottomBar />
     </ChakraProvider>
   );
 }
