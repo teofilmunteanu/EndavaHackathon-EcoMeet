a872e709ca6d22dbb2d040dcaf66890230c21ad2
nicu1249 <tomaalex46@yahoo.com>
2022-09-17 17:29:58 +0300

    SignUp/Login(FaraLinks)
    Daca stiti sa faceti links intre pagini, pls help :)

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import "./App.css";
-
 import * as React from "react";
 import {Route, Routes} from "react-router-dom"
 
 import Description from "./Components/description";
 import NavBar from "./Components/NavBar";
 import UpperHomePage from "./Components/UpperHomePage";
+import OSignUp from "./Components/OSignUp";
+import VSignUp from "./Components/VSignUp";
+import Login from "./Components/Login";
 
 import { ChakraProvider } from "@chakra-ui/react";
 import {mainTheme} from "./styles/theme";
@@ -17,6 +18,9 @@ function App() {
         <NavBar/>
         <Routes>
           <Route path="/" element={<><UpperHomePage/><Description/></>}/>
+          <Route path="/OSignUp" element={<><OSignUp/></>}/>
+          <Route path="/VSignUp" element={<><VSignUp/></>}/>
+          <Route path="/Login" element={<><Login/></>}/>
         </Routes>
     </ChakraProvider>
   );
