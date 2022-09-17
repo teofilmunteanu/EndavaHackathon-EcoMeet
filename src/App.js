b90485148cf93b20d62c7d2af6acc86fa6f64338
Vlad Hailă <99145328+VladHaila@users.noreply.github.com>
2022-09-17 18:03:23 +0300

    Nicu + Vlad merge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Route, Routes} from "react-router-dom"
+import { Route, Routes } from "react-router-dom";
 
 import Description from "./Components/description";
 import NavBar from "./Components/NavBar";
@@ -10,23 +10,49 @@ import Login from "./Components/Login";
 import BottomBar from "./Components/BottomBar";
 
 import { ChakraProvider } from "@chakra-ui/react";
-import {mainTheme} from "./styles/theme";
 
+import { mainTheme } from "./styles/theme";
 
 function App() {
   return (
     <ChakraProvider resetCSS theme={mainTheme}>
-        <NavBar/>
-        <Routes>
           <Route path="/" element={<><UpperHomePage/><Description/></>}/>
-          <Route path="/OSignUp" element={<><OSignUp/></>}/>
-          <Route path="/VSignUp" element={<><VSignUp/></>}/>
-          <Route path="/Login" element={<><Login/></>}/>
-        </Routes>
-    <ChakraProvider>
-      <NavBar></NavBar>
-      <UpperHomePage></UpperHomePage>
       <Description></Description>
+      <NavBar />
+      <Routes>
+        <Route
+          path="/"
+          element={
+              <UpperHomePage></UpperHomePage>
+              <Description></Description>
+            </>
+          }
+        />
+        <Route
+          path="/OSignUp"
+          element={
+            <>
+              <OSignUp />
+            </>
+          }
+        />
+        <Route
+          path="/VSignUp"
+          element={
+            <>
+              <VSignUp />
+            </>
+          }
+        />
+        <Route
+          path="/Login"
+          element={
+            <>
+              <Login />
+            </>
+          }
+        />
+      </Routes>
       <BottomBar></BottomBar>
     </ChakraProvider>
   );
