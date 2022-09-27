0f94698c397904e3f9ec3527350b50f24358b70a
Munteanu Teofil <andreiteofil01@gmail.com>
2022-09-27 13:11:02 +0300

    VSignUp adjustments

--- ReactApp/src/App.js
+++ ReactApp/src/App.js
@@ -11,10 +11,8 @@ import Login from "./Components/Login";
 import NavBarProfile from "./Components/NavBarProfile";
 import CSignUp from "./Components/CSignUp";
 import UProfile from "./Components/UProfile";
-import OProfile from "./Components/OProfile"
+import OProfile from "./Components/OProfile";
 import Shop from "./Components/Shop";
-
-
 
 import { ChakraProvider } from "@chakra-ui/react";
 import { mainTheme } from "./styles/theme";
@@ -25,91 +23,83 @@ import OFeed from "./Components/OFeed";
 import { VFeedData } from "./Components/VFeedData";
 import { OFeedData } from "./Components/OFeedData";
 import { ShopData } from "./Components/ShopData";
-import {ProfileData} from "./Components/ProfileData"
+import { ProfileData } from "./Components/ProfileData";
 
 function App() {
-  return (
-    <ChakraProvider resetCSS theme={mainTheme}>
-      <NavBar />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <UpperHomePage />
-              <Description />
-            </>
-          }
-        />
-        <Route
-          path="/OSignUp"
-          element={
-            <>
-              <OSignUp />
-            </>
-          }
-        />
-        <Route
-          path="/VSignUp"
-          element={
-            <>
-              <VSignUp />
-            </>
-          }
-        />
-        <Route
-          path="/CSignUp"
-          element={
-            <>
-              <CSignUp />
-            </>
-          }
-        />
-        <Route
-          path="/Login"
-          element={
-            <>
-              <Login />
-            </>
-          }
-        />
-        <Route
-          path="/VFeed"
-          element={
-            <>
-              <VFeed feed={VFeedData} />
-            </>
-          }
-        />
-        <Route path="/OFeed" element={<OFeed ofeed={OFeedData} />} />
-        <Route
-          path="/UProfile"
-          element={
-            <>
-              <UProfile feed={VFeedData} />
-            </>
-          }
-        />
-        <Route
-          path="/OProfile"
-          element={
-            <>
-              <OProfile feed={VFeedData} secondfeed={ProfileData}/>
-            </>
-          }
-        />
-      <Route
-          path="/Shop"
-          element={
-            <>
-              <Shop feed={ShopData} />
-            </>
-          }
-        />
-
-        </Routes>
-    </ChakraProvider>
-  );
+	return (
+		<ChakraProvider resetCSS theme={mainTheme}>
+			<NavBar />
+			<Routes>
+				<Route
+					path="/"
+					element={
+						<>
+							<UpperHomePage />
+							<Description />
+						</>
+					}
+				/>
+				<Route
+					path="/OSignUp"
+					element={
+						<>
+							<OSignUp />
+						</>
+					}
+				/>
+				<Route
+					path="/VSignUp"
+					element={
+						<>
+							<VSignUp />
+						</>
+					}
+				/>
+				<Route
+					path="/CSignUp"
+					element={
+						<>
+							<CSignUp />
+						</>
+					}
+				/>
+				<Route
+					path="/Login"
+					element={
+						<>
+							<Login />
+						</>
+					}
+				/>
+				<Route
+					path="/VFeed"
+					element={
+						<>
+							<VFeed feed={VFeedData} />
+						</>
+					}
+				/>
+				<Route path="/OFeed" element={<OFeed ofeed={OFeedData} />} />
+				<Route path="/UProfile" element={<UProfile feed={VFeedData} />} />
+				<Route
+					path="/OProfile"
+					element={
+						<>
+							<OProfile feed={VFeedData} secondfeed={ProfileData} />
+						</>
+					}
+				/>
+				<Route
+					path="/Shop"
+					element={
+						<>
+							<Shop feed={ShopData} />
+						</>
+					}
+				/>
+			</Routes>
+		</ChakraProvider>
+	);
 }
 
 export default App;
--- ReactApp/src/Components/VSignUp.jsx
+++ ReactApp/src/Components/VSignUp.jsx
@@ -1,66 +1,78 @@
-import {Button, Flex, FormControl, FormLabel, Heading, Input, Stack, Image, Divider} from '@chakra-ui/react';
+import {
+	Button,
+	Flex,
+	FormControl,
+	FormLabel,
+	Heading,
+	Input,
+	Stack,
+	Image,
+	Divider,
+} from "@chakra-ui/react";
+import { Formik } from "formik";
 import { ArrowBackIcon } from "@chakra-ui/icons";
+import { Form } from "react-router-dom";
 
 function VSignUp() {
-  return (
-    <Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
-      <Flex p={8} flex={1} align={"center"} justify={"center"}>
-        <Stack spacing={4} w={"full"} maxW={"md"}>
-          <Heading fontSize={["6vw", "4vw", "2.5vw"]} variant="authEffect">
-            Hello, Volunteer!
-          </Heading>
-          <Divider orientation="horizontal" />
-          <FormControl id="username">
-            <FormLabel variant="authEffect">Username:</FormLabel>
-            <Input type="text" />
-          </FormControl>
-          <FormControl id="email">
-            <FormLabel variant="authEffect">Email:</FormLabel>
-            <Input type="email" />
-          </FormControl>
-          <Stack
-            direction={{ base: "column", sm: "row" }}
-            align={"start"}
-            justify={"space-between"}
-          >
-            <FormControl id="password">
-              <FormLabel variant="authEffect">Set Password:</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel variant="authEffect">Confirm Password:</FormLabel>
-              <Input type="password" />
-            </FormControl>
-          </Stack>
-          <FormControl id="town">
-            <FormLabel variant="authEffect">Town/City:</FormLabel>
-            <Input type="text" />
-          </FormControl>
-          <Stack
-            direction={{ base: "line", sm: "row" }}
-            align={"start"}
-            justify={"space-between"}
-          >
-            <Button variant="auth" bg="white"
-            onClick={(e) => {
-              e.preventDefault();
-              window.location.href = "http://localhost:3000/";
-            }}>
-              <ArrowBackIcon />
-              Back
-            </Button>
-            <Button variant="auth"
-            onClick={(e) => {
-              e.preventDefault();
-              window.location.href = "#";
-            }}>Sign up</Button>
-          </Stack>
-        </Stack>
-      </Flex>
-      <Flex flex={1}>
-        <Image alt={"Login Image"} objectFit={"cover"} src={"./SignUp1.png"} />
-      </Flex>
-    </Stack>
-  );
+	return (
+		<Stack minH={"100vh"} direction={{ base: "column", md: "row" }}>
+			<Formik onSubmit={(data) => console.log(data)} initialValues={{}}>
+				{(props) => (
+					<Form>
+						<Flex p={8} flex={1} align={"center"} justify={"center"}>
+							<Stack spacing={4} w={"full"} maxW={"md"}>
+								<Heading fontSize={["6vw", "4vw", "2.5vw"]} variant="authEffect">
+									Hello, Volunteer!
+								</Heading>
+								<Divider orientation="horizontal" />
+								<FormControl id="username">
+									<FormLabel variant="authEffect">Username:</FormLabel>
+									<Input type="text" />
+								</FormControl>
+								<FormControl id="email">
+									<FormLabel variant="authEffect">Email:</FormLabel>
+									<Input type="email" />
+								</FormControl>
+								<Stack
+									direction={{ base: "column", sm: "row" }}
+									align={"start"}
+									justify={"space-between"}
+								>
+									<FormControl id="password">
+										<FormLabel variant="authEffect">Set Password:</FormLabel>
+										<Input type="password" />
+									</FormControl>
+									<FormControl id="password">
+										<FormLabel variant="authEffect">Confirm Password:</FormLabel>
+										<Input type="password" />
+									</FormControl>
+								</Stack>
+								<FormControl id="town">
+									<FormLabel variant="authEffect">Town/City:</FormLabel>
+									<Input type="text" />
+								</FormControl>
+								<Stack
+									direction={{ base: "line", sm: "row" }}
+									align={"start"}
+									justify={"space-between"}
+								>
+									<Button variant="auth" bg="white">
+										<ArrowBackIcon />
+										Back
+									</Button>
+									<Button variant="auth" type="submit">
+										Sign up
+									</Button>
+								</Stack>
+							</Stack>
+						</Flex>
+						<Flex flex={1}>
+							<Image alt={"Login Image"} objectFit={"cover"} src={"./SignUp1.png"} />
+						</Flex>
+					</Form>
+				)}
+			</Formik>
+		</Stack>
+	);
 }
 export default VSignUp;
